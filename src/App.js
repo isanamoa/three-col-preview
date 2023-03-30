48daff68841627f5c06c1504a9f041281089363e
isanamoa <isaacansaha@gmail.com>
2023-03-30 21:51:28 +0000

    Three level components assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Datastore from './components/Datastore';
+import PreviewCard from './components/PreviewCard';
 
 function App() {
+    const [dataItem, setDataItem] = useState([]);
+    
+    useEffect(()=>setDataItem(Datastore), []);
+    
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <div className='box'>
+        
+        <PreviewCard  item={ dataItem } />
+      </div>
+      {
+      
+      /*
+       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +33,8 @@ function App() {
           Learn React
         </a>
       </header>
+       */}
+
     </div>
   );
 }
